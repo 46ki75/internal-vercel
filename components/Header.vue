066aa289befa49d3f39416c38f24ab33eb1f9063
shirayuki <shirayuki@46ki75.com>
2024-11-04 16:56:01 +0900

    feat: update header component with new navigation icons and improved layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,25 @@
 <template>
   <header class="header">
-    <ElmToggleTheme />
-    <ElmLoginIcon
-      :is-loading="isLoading"
-      :is-login="!isError"
-      @click="logout"
-    />
+    <div class="icon-container">
+      <HomeIcon class="icon" @click="$router.push('/')" />
+      <TagIcon class="icon" @click="$router.push('/anki')" />
+      <SwatchIcon class="icon" @click="$router.push('/color')" />
+    </div>
+
+    <div class="icon-container">
+      <ElmToggleTheme />
+      <ElmLoginIcon
+        :is-loading="isLoading"
+        :is-login="!isError"
+        @click="logout"
+      />
+    </div>
   </header>
 </template>
 
 <script setup lang="ts">
 import { ElmLoginIcon, ElmToggleTheme } from '@elmethis/core'
+import { HomeIcon, SwatchIcon, TagIcon } from '@heroicons/vue/24/solid'
 
 const router = useRouter()
 const route = useRoute()
@@ -50,7 +59,7 @@ onMounted(checkSession)
 <style scoped lang="scss">
 .header {
   box-sizing: border-box;
-  padding: 0.5rem;
+  padding: 0 0.75rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -58,5 +67,24 @@ onMounted(checkSession)
   top: 0;
   height: 3rem;
   box-shadow: 0 0 0.25rem rgba(black, 0.2);
+
+  .icon-container {
+    display: flex;
+    gap: 0.5rem;
+
+    .icon {
+      padding: 2px;
+      width: 24px;
+      transition: background-color 200ms;
+      cursor: pointer;
+
+      &:hover {
+        background-color: rgba(black, 0.1);
+        [data-theme='dark'] & {
+          background-color: rgba(white, 0.1);
+        }
+      }
+    }
+  }
 }
 </style>
