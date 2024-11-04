c0a1ef4f607e73f0486d615b1582075de1412655
shirayuki <shirayuki@46ki75.com>
2024-11-04 14:38:10 +0900

    feat: implement default layout with header and integrate Vuetify for styling

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout name="default">
+    <v-app><NuxtPage /></v-app>
+  </NuxtLayout>
 </template>
---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">HEADER</header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  height: 3rem;
+  box-shadow: 0 0 0.25rem rgba(black, 0.2);
+}
+</style>
