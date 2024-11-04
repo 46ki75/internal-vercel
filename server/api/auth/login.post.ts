9987a6fde0229973045399517cd7dea2b180bbba
shirayuki <shirayuki@46ki75.com>
2024-11-04 13:04:19 +0900

    feat: implement password hashing and login functionality with argon2

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,9 +1,17 @@
-import { hash } from 'argon2'
+import { verify } from 'argon2'
+import 'dotenv/config'
 
 export default defineEventHandler(async (event) => {
   setHeader(event, 'Content-Type', 'application/json')
 
   try {
+    const hash = process.env.PASSWORD
+
+    if (hash == null) {
+      setResponseStatus(event, 500)
+      return { error: 'Hashed password is not set' }
+    }
+
     const isValidBody = await readValidatedBody(event, (body) => {
       return typeof body === 'object' && body !== null
     })
@@ -22,7 +30,14 @@ export default defineEventHandler(async (event) => {
       return { error: '`password` is required' }
     }
 
-    return { hash: await hash(password) }
+    const isValid = await verify(hash, password)
+
+    if (!isValid) {
+      setResponseStatus(event, 401)
+      return { error: 'Invalid password' }
+    }
+
+    return { message: 'Success' }
   } catch {
     setResponseStatus(event, 500)
     return { error: 'Internal server error' }
