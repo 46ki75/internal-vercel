4baed54c45db994390287d7f44e388fa4331f09e
shirayuki <shirayuki@46ki75.com>
2024-11-07 15:41:26 +0900

    feat: enhance page transition effects in app.vue with fade animations

--- app.vue
+++ app.vue
@@ -2,7 +2,17 @@
   <ClientOnly>
     <Suspense>
       <template #default>
-        <NuxtLayout name="default"><NuxtPage /></NuxtLayout>
+        <NuxtLayout name="default">
+          <NuxtPage
+            :transition="{
+              enterActiveClass: 'fade-enter-active',
+              leaveActiveClass: 'fade-leave-active',
+              enterFromClass: 'fade-enter-from',
+              leaveToClass: 'fade-leave-to',
+              mode: 'out-in'
+            }"
+          />
+        </NuxtLayout>
       </template>
 
       <template #fallback><Fallback /></template>
@@ -18,5 +28,14 @@ body {
   [data-theme='dark'] & {
     background-color: rgba(0, 0, 0, 0.85);
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 100ms;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
